f403d41770bc2bc27cb42eec0c6ee0766582c66c
Maevskaya Olga <olganma@gmail.com>
2023-02-10 01:15:05 +0300

    Tests improved after fidback

--- src/test/java/NegativeCardTest.java
+++ src/test/java/NegativeCardTest.java
@@ -37,9 +37,11 @@ public class NegativeCardTest {
     void shouldAlertIfLatinName() {
         driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("[data-test-id=\"name\"] .input__control")).sendKeys("Ivan Popov");
+        driver.findElement(By.cssSelector("[data-test-id=\"phone\"] .input__control")).sendKeys("+79261111111");
+        driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
         driver.findElement(By.className("button__text")).click();
         String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
-        String actual = driver.findElement(By.cssSelector("[data-test-id=\"name\"] .input__sub")).getText();
+        String actual = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText();
         assertEquals(expected, actual.trim());
     }
 
@@ -47,9 +49,11 @@ public class NegativeCardTest {
     void shouldAlertIfNoName() {
         driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("[data-test-id=\"name\"] .input__control")).sendKeys("");
+        driver.findElement(By.cssSelector("[data-test-id=\"phone\"] .input__control")).sendKeys("+79261111111");
+        driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
         driver.findElement(By.className("button__text")).click();
         String expected = "Поле обязательно для заполнения";
-        String actual = driver.findElement(By.cssSelector("[data-test-id=\"name\"] .input__sub")).getText();
+        String actual = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText();
         assertEquals(expected, actual.trim());
     }
 
@@ -58,9 +62,10 @@ public class NegativeCardTest {
         driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("[data-test-id=\"name\"] .input__control")).sendKeys("Маевская Ольга");
         driver.findElement(By.cssSelector("[data-test-id=\"phone\"] .input__control")).sendKeys("79261111111");
+        driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
         driver.findElement(By.className("button__text")).click();
         String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
-        String actual = driver.findElement(By.cssSelector("[data-test-id=\"phone\"] .input__sub")).getText();
+        String actual = driver.findElement(By.cssSelector("[data-test-id=\"phone\"].input_invalid .input__sub")).getText();
         assertEquals(expected, actual.trim());
     }
 
@@ -69,9 +74,21 @@ public class NegativeCardTest {
         driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("[data-test-id=\"name\"] .input__control")).sendKeys("Маевская Ольга");
         driver.findElement(By.cssSelector("[data-test-id=\"phone\"] .input__control")).sendKeys("");
+        driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
         driver.findElement(By.className("button__text")).click();
         String expected = "Поле обязательно для заполнения";
-        String actual = driver.findElement(By.cssSelector("[data-test-id=\"phone\"] .input__sub")).getText();
+        String actual = driver.findElement(By.cssSelector("[data-test-id=\"phone\"].input_invalid .input__sub")).getText();
+        assertEquals(expected, actual.trim());
+    }
+
+    @Test
+    void shouldAlertNoAgreement() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=\"name\"] .input__control")).sendKeys("Маевская Ольга");
+        driver.findElement(By.cssSelector("[data-test-id=\"phone\"] .input__control")).sendKeys("+79261111111");
+        driver.findElement(By.className("button__text")).click();
+        String expected = "Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй";
+        String actual = driver.findElement(By.cssSelector(".input_invalid .checkbox__text")).getText();
         assertEquals(expected, actual.trim());
     }
 }
